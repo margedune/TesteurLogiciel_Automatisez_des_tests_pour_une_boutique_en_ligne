a65dcf8dc88a31ffdc192b99448040519a36a6a6
Rova RAHAROSON <margedune@gmail.com>
2024-12-25 12:50:00 +0100

    test Panier

--- cypress/e2e/Panier.cy.js
+++ cypress/e2e/Panier.cy.js
@@ -17,13 +17,13 @@ describe('login page', () => {
                 cy.log(cartSection.find('#cart-content .product'));
             });*/
 
-            // Accéder à la page des produits
-            cy.visit('http://localhost:8080/#/');
-            cy.contains('button', 'Voir les produits').click()
-
-            // Cliquez sur un des produits
+            // Visiter la page du produit
+            // Recuperer le nom du produit
             cy.visit(`http://localhost:8080/#/products/${Id_product}`);
-            cy.get('h1[data-cy="detail-product-name"]').invoke('text').then((text) => {
+            cy.get('[data-cy="detail-product-name"]')
+           
+            .then((text) => {
+                cy.log(text);
                 cy.wrap(text).as('productName');
             });
 
@@ -46,61 +46,8 @@ describe('login page', () => {
             });
         });
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*it('Ajout de produit au panier avec vérifications', () => {
-        // Ajout du produit
-        cy.visit('http://localhost:8080/#/products/5');
-        cy.get('button[data-cy="detail-product-add"]').click();
-            
-        // Vérifier que le produit a été ajouté au panieR
-        cy.get('div[data-cy="cart-line"]').find('input[data-cy="cart-line-quantity"]').contains('1');
-        //cy.get('input[data-cy="cart-line-quantity"]').should('have.length', 1);
-        // Retourner à la page du produit
-        cy.go('back');
-    });*/
 }); 
+    /*aller dans la page panier, je cherche les poubelles, je clique sur la poubelle
+    revenir dans la page produit j'ajoute le produit
+    revenir dans la page panier et vérifier que le produit est bien ajouté
 
-
-/*it('Vérifier que le stock a été mis à jour', () => {
-    cy.visit('http://localhost:8080/#/products/5');
-    cy.get('div[data-cy="product-cart"]').find('p[data-cy="detail-product-stock"]').should(($stock) => {
-    //cy.get('p[data-cy="detail-product-stock"]').should(($stock) => {
-        console.log($stock);
-        const updatedStock = parseInt($stock.text(), 10);
-        expect(updatedStock).to.be.equal($stock - 1);
-    });
-
-
-it('Vérification des limites', () => {
-
-    // Entrer un chiffre négatif
-    cy.get('input[data-cy="detail-product-quantity"]').clear().type('-1');
-    cy.get('button[data-cy="detail-product-add"]').click();
-    cy.get('.error-message').should('contain', 'Quantité invalide');
-
-    // Entrer un chiffre supérieur à 20
-    cy.get('input[data-cy="detail-product-quantity"]').clear().type('21');
-    cy.get('button[data-cy="detail-product-add"]').click();
-    cy.get('.error-message').should('contain', 'Quantité invalide');
-});
-
-});*/
